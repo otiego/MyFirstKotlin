96220d4acbb10507dbe53db941336cb2e2e163ae
OtiegoRonald <51930057+otiego@users.noreply.github.com>
2022-09-10 13:35:35 +0300

    Kotlin files

---
+++ ArithmeticOp.kt
@@ -0,0 +1,8 @@
+
+fun main(){
+    val x = 5
+    val y=2.5
+    println(x/y)
+    val a = 10%3
+    println("The value of a is $a")
+}
---
+++ Variables.kt
@@ -0,0 +1,22 @@
+
+fun main(){
+    var x =5
+    println("The value of x is $x")
+//    with var, we can change the value of a variable
+    x = 7
+    println("The value of x is now $x")
+
+    val y = 2
+//    when we use the keyword val to declare a variable, we can't change the value of that variable in the program
+    println("The value of y is $y")
+
+    val iLoveVegetables = false
+    println("It is $iLoveVegetables I love vegetables")
+
+    val iAmALetter = 'R'
+    println("My first name starts with $iAmALetter")
+
+    val decimal = 3.64f
+    println(decimal)//This is a decimal
+
+}
