0e6ed60cde97eb59851e043b44ad19c9c40b9ca3
Otiego <ronald.otiego@gmail.com>
2022-09-12 06:48:48 +0300

    Updated the variable and ArithmeticOp.kt

--- src/ArithmeticOp.kt
+++ src/ArithmeticOp.kt
@@ -3,6 +3,13 @@ fun main(){
     val x = 5
     val y=2.5
     println(x/y)
+
     val a = 10%3
     println("The value of a is $a")
+
+    val d = 7*6
+    println("The value of d is $d")
+
+    val s = 5%2
+    print("The value of s is $s")
 }
--- src/Variables.kt
+++ src/Variables.kt
@@ -19,4 +19,8 @@ fun main(){
     val decimal = 3.64f
     println(decimal)//This is a decimal
 
+//    In kotlin we don't need to explicitly define a data type during variable declaration
+    val iLovePizza = true
+    println("It is $iLovePizza that I love Pizza ")
+
 }
